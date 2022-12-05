1248855f6192983fa19c1d2933dd060e6ba2b36e
thomas-spinec <thomas.spinec@laplateforme.io>
2022-12-05 10:34:16 +0100

    récupération des pages du projet 'module_connexion' en vue de les modifier pour le projet 'livreor'

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+// Connexion à la base de données en local
+
+$connect = new mysqli('localhost', 'root', '', 'moduleconnexion');
+
+// Connexion à la base de données en ligne
+
+// $connect = new mysqli('localhost', 'Thomas', 'mdpmoduleco', 'thomas-spinec_moduleconnexion')
+?>
