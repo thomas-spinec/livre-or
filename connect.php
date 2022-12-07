de90104fdd50cc2dd1f6f647dbe79d2a97192fe3
thomas-spinec <thomas.spinec@laplateforme.io>
2022-12-07 14:18:39 +0100

    correction du MDP de connexion pour plesk

--- connect.php
+++ connect.php
@@ -5,5 +5,5 @@ $connect = new mysqli('localhost', 'root', '', 'livreor');
 
 // Connexion à la base de données en ligne
 
-// $connect = new mysqli('localhost', 'adminbdd', 'basededonnées', 'thomas-spinec_livreor')
+// $connect = new mysqli('localhost', 'adminbdd', 'basededonnees', 'thomas-spinec_livreor')
 ?>
