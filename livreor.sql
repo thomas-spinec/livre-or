ecc04d84849e10f2c14caa1d2a7bf89fd1989039
thomas-spinec <thomas.spinec@laplateforme.io>
2022-12-08 12:22:09 +0100

    test de changement dans la BDD pour les accents

--- livreor.sql
+++ livreor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 07 déc. 2022 à 13:58
+-- Généré le : jeu. 08 déc. 2022 à 11:35
 -- Version du serveur : 10.6.11-MariaDB-0ubuntu0.22.04.1
 -- Version de PHP : 8.1.2-1ubuntu2.9
 
@@ -32,20 +32,20 @@ CREATE TABLE `commentaires` (
   `commentaire` text NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Déchargement des données de la table `commentaires`
 --
 
 INSERT INTO `commentaires` (`id`, `commentaire`, `id_utilisateur`, `date`) VALUES
-(2, 'Ceci est un test pour la date', 2, '2022-12-05 00:00:00'),
-(3, 'Ce site est propre', 3, '2022-12-05 00:00:00'),
-(4, 'La modération c&#039;est top', 1, '2022-12-05 00:00:00'),
-(5, 'J&#039;ai bien morpionné, merci!', 4, '2022-12-05 00:00:00'),
-(6, 'J&#039;ai mangé du pain hier', 5, '2022-12-05 00:00:00'),
-(7, 'Sympa cette idée de livre d&#039;or!', 2, '2022-12-05 00:00:00'),
-(8, 'Test 2 date', 2, '2022-12-06 15:39:01');
+(1, 'écrasement de la base de données pour test', 1, '2022-12-08 11:30:43'),
+(2, 'Sympa cette idée de livre d&#039;or!', 2, '2022-12-08 11:31:37'),
+(3, 'J&#039;ai mangé du pain hier', 3, '2022-12-08 11:32:12'),
+(4, 'Ce site est propre!', 4, '2022-12-08 11:33:03'),
+(5, 'J&#039;ai bien morpionné, merci!', 5, '2022-12-08 11:33:49'),
+(6, 'la modération c&#039;est top!', 1, '2022-12-08 11:34:18'),
+(7, 'Deuxième test', 2, '2022-12-08 11:34:55');
 
 -- --------------------------------------------------------
 
@@ -57,18 +57,18 @@ CREATE TABLE `utilisateurs` (
   `id` int(11) NOT NULL,
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
-(1, 'admin', '$2y$10$l.o3B3GBY.b25beWaaq8Qup57GDFlue8G24K1z1EboYt88QcmJk2S'),
-(2, 'Tom', '$2y$10$n8gu3HiQzDMqa294kSDmx.lGqseTHnkauR4G3E8hUwPk4e0b215fu'),
-(3, 'kami', '$2y$10$5RoY14Xjsn8sDOlPwwJfN.sDOUpT5kWND7i4mQIa8vBruv9G4xA4S'),
-(4, 'nadou', '$2y$10$5zBzjB7pnGyB59Do71X/ROaW1pKRjmb318zwdjgvdpjOIiHDn2WzO'),
-(5, 'abc', '$2y$10$7B6/WcvcJ8wf6yg0nEtdru51L9Vk4lZ.bg.QPKb57zrAFxT0EeG2O');
+(1, 'admin', '$2y$10$sN2Novn37oVKBJi5r6Hw4e3cy7i32EcHz/hWT4P9T1fWm3ju4/72i'),
+(2, 'Tom', '$2y$10$CtfVAGWL22Z8aO6An3VhzOXjnRhvuzYSbtTXYNxBZmvWAPhSyCvGy'),
+(3, 'abc', '$2y$10$LpHVQwezJ3g40qhRcmJZ/u6xZH6Xw3KursQx/6vTUQLOmmljaQhV.'),
+(4, 'kami', '$2y$10$32IjPZvpVvPd9hiICl77peLrZmwwwZYNRtsQAlrSFB5g4qntg2hPC'),
+(5, 'nadou', '$2y$10$296EPsSdr7j1IEJeDDasfu5Y1hIldL7/yVNu3OD5gyBYXYUTW3596');
 
 --
 -- Index pour les tables déchargées
@@ -94,7 +94,7 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `commentaires`
 --
 ALTER TABLE `commentaires`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
